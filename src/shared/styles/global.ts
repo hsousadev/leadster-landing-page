4e031d2cb55fc96bd5a57c5957615b3f4cecfa0b
Henrique Sousa <washington.sousa01@fatec.sp.gov.br>
2023-06-23 17:01:30 -0300

    Initial commit from Create Next App

---
+++ src/shared/styles/global.ts
@@ -0,0 +1,137 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle<any>`
+
+  :root {
+    --Max-content-width: 1140px;
+
+    --Blue: #1B7EFF;
+    --FontColor: #1C3C50;
+    --LightBlue: #F0F8FF;
+    --White: #F9F9F9;
+  }
+
+  html {
+    transition: all 0.2s ease-in-out;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: var(--White);
+
+    @media(max-width: 1360px) {
+      padding: 0 64px;
+    }
+
+    @media(max-width: 720px) {
+      padding: 0 32px;
+
+      h1 {
+        font-size: 32px;
+      }
+
+      h2 {
+        font-size: 24px;
+      }
+
+      h3 {
+        font-size: 16px;
+      }
+
+      h4 {
+        font-size: 14px;
+      }
+
+      p {
+        font-size: 12px;
+      }
+    }
+
+    @media (max-width: 540px) {
+      h1 {
+        font-size: 24px;
+      }
+
+      h2 {
+        font-size: 16px;
+      }
+
+      h3 {
+        font-size: 14px;
+      }
+
+      h4 {
+        font-size: 12px;
+      }
+
+      p {
+        font-size: 10px;
+      }
+    }
+
+    @media(max-width: 425px) {
+      padding: 0 16px;
+    }
+
+    @media(max-width: 1380px) {
+      zoom: 80%;
+    }
+
+    @media(max-width: 1232px) {
+      zoom: 100%;
+    }
+  }
+
+  body {
+    max-width: var(--Max-content-width);
+    width: 100%;
+    background: var(--White) !important;
+    margin: 0;
+    overflow-x: hidden;
+  }
+
+  h1, h2, h3, h4, p, strong {
+    transition: all 0.2s ease-in-out;
+
+    margin: 0;
+    font-family: 'Plus Jakarta Sans', sans-serif;
+    font-style: normal;
+    color: var(--Gray);
+  }
+
+  h1 {
+    font-weight: 600;
+    font-size: 48px;
+  }
+
+  h2 {
+    font-weight: 400;
+    font-size: 32px;
+  }
+
+  h3 {
+    font-weight: 600;
+    font-size: 24px;
+  }
+
+  h4 {
+    font-weight: 400;
+    font-size: 16px;
+  }
+
+  p {
+    font-weight: 300;
+    font-size: 14px;
+  }
+
+  button {
+    cursor: pointer;
+    outline: none;
+    appearance: none;
+    border: none;
+    background: transparent;
+    padding: 0;
+  }
+
+`;
